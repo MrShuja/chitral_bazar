6b17883a13be18222bbb0f6fa98a39e7cc0c9637
mrshuja <mr.react.dvlper@gmail.com>
2025-11-08 17:57:36 +0500

    add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-6xl font-bold text-blue-700 text-center">Starter project with Tailwind css</h1>
+    <div>
+      <Header />
     </div>
   );
 }
